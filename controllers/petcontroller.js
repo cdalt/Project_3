53adc1c01bfe6233333387971d3db2db1117868f
Dalton Natale <cdalton.natale@gmail.com>
2020-10-11 16:37:40 -0700

    updated api routes / token key

--- controllers/petcontroller.js
+++ controllers/petcontroller.js
@@ -7,6 +7,7 @@ module.exports = {
       .catch((err) => res.status(422).json(err));
   },
   findById: function (req, res) {
+    console.log(req.params.id);
     db.Pet.findById(req.params.id)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
