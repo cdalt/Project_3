27e7647fc84b21763bc3850b75d7e16efc22f35f
Blake Ehler <ehlerblake@gmail.com>
2020-10-12 14:44:33 -0700

    Added second package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Form from './components/Form';
+import Form from './components/Form/form';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Form />
+    </div>
   );
 }
 
