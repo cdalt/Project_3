dda6f2d9401dc973522ac027e0905302589016a9
Blake Ehler <ehlerblake@gmail.com>
2020-10-13 13:20:13 -0700

    Basic style for signup/login

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar} from 'react-bootstrap'
 
 import Login from "./components/Login/login.component";
 import SignUp from "./components/SignUp/signup.component";
 
 function App() {
   return (<Router>
-    <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="container">
+    
+      <Navbar bg="primary" variant="dark">
           <Link className="navbar-brand" to={"/sign-in"}>Petfinder</Link>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto">
@@ -21,21 +21,28 @@ function App() {
               </li>
             </ul>
           </div>
-        </div>
-      </nav>
+      </Navbar>
 
-      <div className="auth-wrapper">
-        <div className="auth-inner">
-          <Switch>
-            <Route exact path='/' component={Login} />
-            <Route path="/sign-in" component={Login} />
-            <Route path="/sign-up" component={SignUp} />
-          </Switch>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-6"></div>
+          <div className="col-6 pt-5">
+            <div className="auth-wrapper">
+              <div className="auth-inner">
+                <Switch>
+                  <Route exact path='/' component={Login} />
+                  <Route path="/sign-in" component={Login} />
+                  <Route path="/sign-up" component={SignUp} />
+                </Switch>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div></Router>
+    </Router>
   );
-  
+
 }
 
 export default App;
