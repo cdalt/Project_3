736aca18999f7c6dedb9ce6e6dbbcdb745c026ed
Jermdude7 <jeremys.g.greenfield@gmail.com>
2020-10-14 11:21:21 -0700

    jeremy branch p1

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Home from "./home";
-import about from "./about";
+import Home from "./home.js";
+import about from "./about.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(<App />, document.getElementById('root'));
 
