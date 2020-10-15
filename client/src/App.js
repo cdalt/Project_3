3a7ef90c79a25a5281400e95cee5d5f31d517b8b
Blake Ehler <ehlerblake@gmail.com>
2020-10-15 14:20:00 -0700

    trying to fix this router >:(

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar} from 'react-bootstrap'
 
-import Login from "./components/Login/login.component";
-import SignUp from "./components/SignUp/signup.component";
+import Login from './pages/login'
+import Signup from './pages/signin'
 
 function App() {
   return (<Router>
-      <Navbar bg="primary" variant="dark">
-          <Link className="navbar-brand" to={"/sign-in"}>Petfinder</Link>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>Login</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
-              </li>
-            </ul>
-          </div>
-      </Navbar>
-
-
-      <div className="container">
-        <div className="row">
-          <div className="col-6"></div>
-          <div className="col-6 pt-5">
-            <div className="auth-wrapper">
-              <div className="auth-inner">
-                <Switch>
-                  <Route exact path='/' component={Login} />
-                  <Route path="/sign-in" component={Login} />
-                  <Route path="/sign-up" component={SignUp} />
-                </Switch>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Router>
+     <Route exact path='/' component={Login} />
+     <Route path='/sign-in' component={Login} />
+     <Route path='/sign-up' component={Signup} />
+        </Router>
   );
 
 }
-
-
-
 export default App;
