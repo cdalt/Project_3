5ae117578ff3a1d4b7cd1ede2dca604b14329041
alfredo pedroza <alfredopedrozahernandez13@gmail.com>
2020-10-18 21:06:12 -0700

    asd

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -36,106 +36,87 @@ function Home() {
   };
 
   return (
-    <div className="container">
-      <div class="jumbotron ">
-        <h1 className="display-4 text-center">
-          {" "}
-          Search for the pet of your dreams
-        </h1>
-        <p class="lead text-center">
-          Use the search tool below to find available pets in your area. Search
-          by animal type and breed.
-        </p>
-        <hr class="my-4"></hr>
-        <div class="row d-flex justify-content-center">
-          <div class="col-md-6">
-            {/* <h2 className="title search"> Search Parameters:</h2> */}
+    <div className="big-div">
+      <div className="hero text-center">
+        <h1 className="title"> Search for the pet of your dreams</h1>
 
-            <form>
-              <div class="form-group">
-                <label>Type of Animal</label>
-                <input
-                  value={animalType}
-                  onChange={(event) => handleInputChange(setAnimalType, event)}
-                  class="form-control"
-                  type="text"
-                  placeholder="dog"
-                ></input>
-                <label>Breed of Animal</label>
-                <input
-                  value={animalBreed}
-                  onChange={(event) => handleInputChange(setAnimalBreed, event)}
-                  class="form-control"
-                  type="text"
-                  placeholder="pug"
-                ></input>
-                <label>Zipcode</label>
-                <input
-                  value={animalZipcode}
-                  onChange={(event) =>
-                    handleInputChange(setAnimalZipcode, event)
-                  }
-                  class="form-control"
-                  type="text"
-                  placeholder="20878"
-                ></input>
-              </div>
-              <p class="lead">
-                <button
-                  class="btn btn-primary btn-lg"
-                  onClick={handleSubmit}
-                  type="submit"
-                  role="button"
-                >
-                  Search
-                </button>
-              </p>
-            </form>
-          </div>
-        </div>
-        <hr class="my-4"></hr>
-        <div class="row">
-          <div class="col-sm-12">
-            {!animalResults && (
-              <h3 className="">Pets will appear here in a moment..</h3>
-            )}
-            <div class="row">
-              {animalResults &&
-                animalResults.map((animalResult) => {
-                  return (
-                    <div class=" col-sm-4 card" id="petCard1">
-                      <img
-                        src={animalResult.primary_photo_cropped.full}
-                        class="card-img-top"
-                        id="petImage1"
-                        alt="..."
-                      ></img>
-                      <div class="card-body">
-                        <h5 class="card-title" id="animalName1">
-                          {animalResult.name}
-                        </h5>
-                        <p class="card-text" id="petInfo1">
-                          <p id="animalInfo1">type: {animalResult.type}</p>
-                          <p id="breedInfo1">breed: {animalResult.breed}</p>
-                          <p id="sizeInfo1">size: {animalResult.size}</p>
-                          <p id="locationInfo1">
-                            distance: {animalResult.distance}
-                          </p>
-                          <p id="shelterInfo1">Info on the shelter</p>
-                        </p>
-                        <a
-                          href={animalResult.url}
-                          class="btn btn-primary"
-                          id="link1"
-                        >
-                          See where they are now
-                        </a>
-                      </div>
-                    </div>
-                  );
-                })}
+        <div class="column-md-6">
+          <h2 className="title search"> Search Parameters:</h2>
+
+          <form>
+            <div class="form-group col-md-4">
+              <label for="typeOfAnimal"></label>
+              <input
+                value={animalType}
+                onChange={(event) => handleInputChange(setAnimalType, event)}
+                class="form-control"
+                type="text"
+                placeholder="Type of animal"
+              ></input>
+              <label for="Breed of animal"></label>
+              <input
+                value={animalBreed}
+                onChange={(event) => handleInputChange(setAnimalBreed, event)}
+                class="form-control"
+                type="text"
+                placeholder="Breed of animal"
+              ></input>
+              <label for="zipcode"></label>
+              <input
+                value={animalZipcode}
+                onChange={(event) => handleInputChange(setAnimalZipcode, event)}
+                class="form-control"
+                type="text"
+                placeholder="Zipcode"
+              ></input>              <button
+                onClick={handleSubmit}
+                type="submit"
+                class="btn btn-primary"
+              >
+                Search
+              </button>
+                <a href={{empty}} class="btn btn-primary" id="search">
+                Search
+                </a>
             </div>
-
+          </form>
+        </div>
+        <div class="column-md-12">
+          <h2 className="title">Pets will appear here in a moment..</h2>
+          <div class="row">
+            {animalResults.map((animalResult) => {
+              return (
+                <div class=" col-sm-4 card" id="petCard1">
+                  <img
+                    src={animalResult.primary_photo_cropped.full}
+                    class="card-img-top"
+                    id="petImage1"
+                    alt="..."
+                  ></img>
+                  <div class="card-body">
+                    <h5 class="card-title" id="animalName1">
+                      {animalResult.name}
+                    </h5>
+                    <p class="card-text" id="petInfo1">
+                      <p id="animalInfo1">type: {animalResult.type}</p>
+                      <p id="breedInfo1">breed: {animalResult.breed}</p>
+                      <p id="sizeInfo1">size: {animalResult.size}</p>
+                      <p id="locationInfo1">
+                        distance: {animalResult.distance}
+                      </p>
+                      <p id="shelterInfo1">Info on the shelter</p>
+                    </p>
+                    <a
+                      href={animalResult.url}
+                      class="btn btn-primary"
+                      id="link1"
+                    >
+                      See where they are now
+                    </a>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
