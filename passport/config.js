96b9d5db9d108b977f7abdd02cc9c2e77ee60f43
Blake Ehler <ehlerblake@gmail.com>
2020-10-18 20:47:23 -0700

    formatted code

--- passport/config.js
+++ passport/config.js
@@ -3,10 +3,7 @@ var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 require("dotenv").config();
 require("../passport/config.js");
 const User = require("../models/user");
-// Use the GoogleStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and Google
-//   profile), and invoke a callback with a user object.
+
 passport.use(
   new GoogleStrategy(
     {
