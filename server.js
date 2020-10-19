c7299e420754975edd892cb7cae99001c9cf7df8
Blake Ehler <ehlerblake@gmail.com>
2020-10-19 08:54:58 -0700

    Added mongodb connection

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const passport = require("passport");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 require("dotenv").config();
+const MongoClient = require('mongodb').MongoClient;
 
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
@@ -17,6 +18,9 @@ app.use(
     keys: [process.env.cookieKey],
   })
 );
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(routes);
@@ -38,6 +42,15 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/petfinder", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+const uri = "mongodb+srv://bugettrackerdb:PkANEChpRB2I1LKE@find-a-pet.wmvvx.mongodb.net/petfinder?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
