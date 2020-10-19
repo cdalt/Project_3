7a2fec1820520b28b0b3abbe4a9659e804f9e36f
Blake Ehler <67291509+Blakeehhler@users.noreply.github.com>
2020-10-19 11:22:56 -0700

    Google auth fixes (#47)
    * Added login page fixes
    
    * got the app to npm start
    
    * fixed login page
    
    * state added to signup and login
    
    * pull
    
    * Formatted code
    
    * f
    
    * trying to fix this router >:(
    
    * hp
    
    * Homepage (#25)
    
    * pull
    
    * hp
    
    Co-authored-by: Dalton Natale <cdalton.natale@gmail.com>
    
    * fixes
    
    * changed callback route, fixed the tokenerror, routed to homepage after login (#26)
    
    Co-authored-by: Dalton Natale <cdalton.natale@gmail.com>
    
    * Dalton (#28)
    
    * changed callback route, fixed the tokenerror, routed to homepage after login
    
    * changed callback route, fixed the tokenerror, routed to homepage after login (#26)
    
    Co-authored-by: Dalton Natale <cdalton.natale@gmail.com>
    
    * added signup and login state (#27)
    
    * Added login page fixes
    
    * got the app to npm start
    
    * fixed login page
    
    * state added to signup and login
    
    * Formatted code
    
    * f
    
    * trying to fix this router >:(
    
    * fixes
    
    Co-authored-by: Dalton Natale <cdalton.natale@gmail.com>
    Co-authored-by: cdalt <68070897+cdalt@users.noreply.github.com>
    
    * .
    
    * f
    
    * f
    
    * fixing routes and adding them to their own page
    
    * d
    
    * Added Navbar to home page and about us page, minor styling for home and about us page
    
    * formated
    
    * formatted code
    
    * Added deployment fixes
    
    * got rid of extra div
    
    * Added mongodb connection
    
    * added routes
    
    * html
    
    * Connectiopn fix
    
    * Connection fix
    
    * conection fixes
    
    Co-authored-by: Dalton Natale <cdalton.natale@gmail.com>
    Co-authored-by: Jermdude7 <67980961+Jermdude7@users.noreply.github.com>
    Co-authored-by: cdalt <68070897+cdalt@users.noreply.github.com>

--- client/src/components/SignUp/signup.component.js
+++ client/src/components/SignUp/signup.component.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import { Link } from "react-router-dom";
 
 export default class SignUp extends Component {
   constructor(props) {
